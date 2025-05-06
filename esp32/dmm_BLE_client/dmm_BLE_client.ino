7fad4111988f973b58c2e74738ed089121824b81
Igor <90918603+IgrekLg@users.noreply.github.com>
2025-05-06 10:41:31 +0300

    Update dmm_BLE_client.ino
    Replacing “std::string value = pRemoteCharacteristic->readValue();” with “String value = pRemoteCharacteristic->readValue();”.

--- esp32/dmm_BLE_client/dmm_BLE_client.ino
+++ esp32/dmm_BLE_client/dmm_BLE_client.ino
@@ -117,7 +117,7 @@ bool connectToServer (void)
 
   // Read the value of the characteristic.
   if (pRemoteCharacteristic->canRead()) {
-    std::string value = pRemoteCharacteristic->readValue();
+    String value = pRemoteCharacteristic->readValue();
     //Serial.print("The characteristic value was: ");
     //Serial.println(value.c_str());
   }
